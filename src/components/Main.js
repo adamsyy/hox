0d2602b53a6b904f9ad6ec08932aa16c4ed221de
adamsy <adamoommenjacob@gmail.com>
2021-11-27 16:17:25 +0530

    packages

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,88 +1,135 @@
+import DStorage from '../abis/DStorage.json'
 import React, { Component } from 'react';
-import { convertBytes } from './helpers';
-import moment from 'moment'
+import Navbar from './Navbar'
+import Main from './Main'
+import Web3 from 'web3';
+import './App.css';
 
-class Main extends Component {
+const ipfsClient = require('ipfs-http-client')
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' }) 
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+   
+    const networkId = await web3.eth.net.getId()
+    const networkData = DStorage.networks[networkId]
+    if(networkData) {
+ 
+      const dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+      this.setState({ dstorage })
+    
+      const filesCount = await dstorage.methods.fileCount().call()
+      this.setState({ filesCount })
+  
+      for (var i = filesCount; i >= 1; i--) {
+        const file = await dstorage.methods.files(i).call()
+        this.setState({
+          files: [...this.state.files, file]
+        })
+      }
+    } else {
+      window.alert('DStorage contract not deployed to detected network.')
+    }
+  }
+
+  captureFile = event => {
+    event.preventDefault()
+
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+
+    reader.readAsArrayBuffer(file)
+    reader.onloadend = () => {
+      this.setState({
+        buffer: Buffer(reader.result),
+        type: file.type,
+        name: file.name
+      })
+      console.log('buffer', this.state.buffer)
+    }
+  }
+
+  uploadFile = description => {
+    console.log("Submitting file to IPFS...")
+
+    ipfs.add(this.state.buffer, (error, result) => {
+      console.log('IPFS result', result.size)
+      if(error) {
+        console.error(error)
+        return
+      }
+
+      this.setState({ loading: true })
+     
+      if(this.state.type === ''){
+        this.setState({type: 'none'})
+      }
+      this.state.dstorage.methods.uploadFile(result[0].hash, result[0].size, this.state.type, this.state.name, description).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.setState({
+         loading: false,
+         type: null,
+         name: null
+       })
+       window.location.reload()
+      }).on('error', (e) =>{
+        window.alert('Error')
+        this.setState({loading: false})
+      })
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      dstorage: null,
+      files: [],
+      loading: false,
+      type: null,
+      name: null
+    }
+    this.uploadFile = this.uploadFile.bind(this)
+    this.captureFile = this.captureFile.bind(this)
+  }
 
   render() {
     return (
-      <div className="container-fluid mt-5 text-center">
-        <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
-            <div className="content">
-              <p>&nbsp;</p>
-              <div className="card mb-3 mx-auto bg-dark" style={{ maxWidth: '512px' }}>
-                <h2 className="text-white text-monospace bg-dark"><b>Hox it today!</b></h2>
-                  <form onSubmit={(event) => {
-                    event.preventDefault()
-                    const description = this.fileDescription.value
-                    this.props.uploadFile(description)
-                  }} >
-                      <div className="form-group">
-                        <br></br>
-                          <input
-                            id="fileDescription"
-                            type="text"
-                            ref={(input) => { this.fileDescription = input }}
-                            className="form-control text-monospace"
-                            placeholder="description..."
-                            required />
-                      </div>
-                    <input type="file" onChange={this.props.captureFile} className="text-white text-monospace"/>
-                    <button type="submit" className="btn-primary btn-block"><b>Upload!</b></button>
-                  </form>
-              </div>
-              <p>&nbsp;</p>
-              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px'}}>
-                <thead style={{ 'fontSize': '15px' }}>
-                  <tr className="bg-dark text-white">
-                    <th scope="col" style={{ width: '10px'}}>id</th>
-                    <th scope="col" style={{ width: '200px'}}>name</th>
-                    <th scope="col" style={{ width: '230px'}}>description</th>
-                    <th scope="col" style={{ width: '120px'}}>type</th>
-                    <th scope="col" style={{ width: '90px'}}>size</th>
-                    <th scope="col" style={{ width: '90px'}}>date</th>
-                    <th scope="col" style={{ width: '120px'}}>uploader/view</th>
-                    <th scope="col" style={{ width: '120px'}}>hash/view/get</th>
-                  </tr>
-                </thead>
-                { this.props.files.map((file, key) => {
-                  return(
-                    <thead style={{ 'fontSize': '12px' }} key={key}>
-                      <tr>
-                        <td>{file.fileId}</td>
-                        <td>{file.fileName}</td>
-                        <td>{file.fileDescription}</td>
-                        <td>{file.fileType}</td>
-                        <td>{convertBytes(file.fileSize)}</td>
-                        <td>{moment.unix(file.uploadTime).format('h:mm:ss A M/D/Y')}</td>
-                        <td>
-                          <a
-                            href={"https://etherscan.io/address/" + file.uploader}
-                            rel="noopener noreferrer"
-                            target="_blank">
-                            {file.uploader.substring(0,10)}...
-                          </a>
-                         </td>
-                        <td>
-                          <a
-                            href={"https://ipfs.infura.io/ipfs/" + file.fileHash}
-                            rel="noopener noreferrer"
-                            target="_blank">
-                            {file.fileHash.substring(0,10)}...
-                          </a>
-                        </td>
-                      </tr>
-                    </thead>
-                  )
-                })}
-              </table>
-            </div>
-          </main>
-        </div>
+      <div>
+        <Navbar account={this.state.account} />
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main
+              files={this.state.files}
+              captureFile={this.captureFile}
+              uploadFile={this.uploadFile}
+            />
+        }
       </div>
     );
   }
 }
 
-export default Main;
+export default App;
